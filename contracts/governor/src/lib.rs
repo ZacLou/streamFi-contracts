75979d4f1529118852e7ac9e3cd6caacaf642a15
Joel Joseph <talk2joeljoseph@gmail.com>
2026-08-31 15:43:47 +0100

    fix: Bug: `DripGovernor::transfer_authority` with `new_authority  (#359)

--- contracts/governor/src/lib.rs
+++ contracts/governor/src/lib.rs
@@ -303,7 +303,8 @@ impl DripGovernor {
         caller: Address,
         new_authority: Address,
     ) -> Result<(), Error> {
-        if is_zero_address(&env, &new_authority)
+        if new_authority == caller
+            || is_zero_address(&env, &new_authority)
             || role::has_role(&env, Role::Admin, &new_authority)
         {
             return Err(Error::InvalidParam);
